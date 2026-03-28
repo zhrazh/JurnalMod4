bc2d3877657e88dc4e883711a0641b34bc59c160
zhrazh <zharaaaaa0@gmail.com>
2026-03-28 08:06:48 +0700

    second commit

---
+++ Modul4_103022400015/KodePaket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+class KodePaket
+{
+    private Dictionary<string, string> tabelKodePaket = new Dictionary<string, string>()
+    {
+        {"Basic", "P201"},
+        {"Standard", "P202"},
+        {"Premium", "P203"},
+        {"Unlimited", "P204"},
+        {"Gaming", "P205"},
+        {"Streaming", "P206"},
+        {"Family", "P207"},
+        {"Business", "P208"},
+        {"Student", "P209"},
+        {"Traveler", "P2010"}
+    };
+
+    public string getKodePaket (string namaPaket)
+    {
+        return tabelKodePaket[namaPaket];
+    }
+}
--- Modul4_103022400015/Program.cs
+++ Modul4_103022400015/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePaket k = new KodePaket();
+            Console.WriteLine("Kode Paket Basic: " + k.getKodePaket("Basic"));
+            Console.WriteLine("Kode Paket Standard: " + k.getKodePaket("Standard"));
+            Console.WriteLine("Kode Paket Premium: " + k.getKodePaket("Premium"));
+            Console.WriteLine("Kode Paket Unlimited: " + k.getKodePaket("Unlimited"));
+            Console.WriteLine("Kode Paket Gaming: " + k.getKodePaket("Gaming"));
+            Console.WriteLine("Kode Paket Streaming: " + k.getKodePaket("Streaming"));
+            Console.WriteLine("Kode Paket Family: " + k.getKodePaket("Family"));
+            Console.WriteLine("Kode Paket Business: " + k.getKodePaket("Business"));
+            Console.WriteLine("Kode Paket Student: " + k.getKodePaket("Student"));
+            Console.WriteLine("Kode Paket Traveler: " + k.getKodePaket("Traveler"));
+        }
+    }
+}
